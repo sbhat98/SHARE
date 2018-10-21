d9bbd8f0bd071bea4d647b3472031d2f4e2077ba
JoshMJ37 <jmjaffe37@gatech.edu>
2018-10-21 06:52:30 -0400

    Linked Registration up through search

--- app/src/main/java/edu/gatech/hackgt/effishare/CreateCommunity.java
+++ app/src/main/java/edu/gatech/hackgt/effishare/CreateCommunity.java
@@ -1,13 +1,39 @@
 package edu.gatech.hackgt.effishare;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateCommunity extends AppCompatActivity {
+    private Button create;
+
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    Context curr_ctx = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_community);
+
+        create = (Button) findViewById(R.id.button6);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSearchItem();
+            }
+        });
+
     }
+
+    public void openSearchItem() {
+        Intent intentCreate = new Intent(curr_ctx, SearchItemScreen.class);
+        curr_ctx.startActivity(intentCreate);
+    }
+
 }

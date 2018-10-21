9f93e60f0769555af040786e6ee24fe1144b7d3b
JoshMJ37 <jmjaffe37@gatech.edu>
2018-10-21 01:44:44 -0400

    added remaning activities

---
+++ app/src/main/java/edu/gatech/hackgt/effishare/Checked_out.java
@@ -0,0 +1,13 @@
+package edu.gatech.hackgt.effishare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Checked_out extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checked_out);
+    }
+}

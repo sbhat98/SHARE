9ae8939b0b755bf0c0500cfca3e0ba4d8846e49f
JoshMJ37 <jmjaffe37@gatech.edu>
2018-10-21 01:16:58 -0400

    Added <add item> activity

--- app/src/main/java/edu/gatech/hackgt/effishare/Add_Item.java
+++ app/src/main/java/edu/gatech/hackgt/effishare/Add_Item.java
@@ -3,10 +3,11 @@ package edu.gatech.hackgt.effishare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class JoinCommunityScreen extends AppCompatActivity {
+public class Add_Item extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join_community_screen);
+        setContentView(R.layout.activity_add__item);
     }
 }

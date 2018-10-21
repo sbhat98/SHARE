40b90648690851e0f78360caa22818b784732206
JoshMJ37 <jmjaffe37@gatech.edu>
2018-10-21 04:02:47 -0400

    fixed lack of Add_Item.java

---
+++ app/src/main/java/edu/gatech/hackgt/effishare/Add_Item.java
@@ -0,0 +1,13 @@
+package edu.gatech.hackgt.effishare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__item);
+    }
+}

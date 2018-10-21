0409c970b08875bb127cb5cfd6673f6cccb06674
Shishir Bhat <shishirvbhat@gmail.com>
2018-10-21 01:46:12 -0400

    Added databaseUtils class

---
+++ app/src/main/java/edu/gatech/hackgt/effishare/DatabaseUtils.java
@@ -0,0 +1,71 @@
+package edu.gatech.hackgt.effishare;
+
+import android.support.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class DatabaseUtils {
+
+    private static final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+    private static List<Item> checkedList;
+
+    public static List<Item> getCheckedOutItems(String userUUID) {
+        ref.child("users").child(userUUID).child("checkedOutItems").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                checkedList =  dataSnapshot.getValue(List.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        while (checkedList == null) {
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        List<Item> retVal = checkedList;
+        checkedList = null;
+        return retVal;
+    }
+
+    private static String communityString;
+
+    public static String getCommunity(String userUUID) {
+        ref.child("users").child(userUUID).child("community").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                communityString =  dataSnapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        while (communityString == null) {
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String retVal = communityString;
+        communityString = null;
+        return retVal;
+    }
+
+
+    private DatabaseUtils() {}
+}

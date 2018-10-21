f5c27abfa2bf80533cbe58c1c1e42308d49b943b
Sam <sam.scheele@outlook.com>
2018-10-21 04:26:54 -0400

    Add chat stuff

--- app/src/main/java/edu/gatech/hackgt/effishare/ChatActivity.java
+++ app/src/main/java/edu/gatech/hackgt/effishare/ChatActivity.java
@@ -67,11 +67,11 @@ public class ChatActivity extends AppCompatActivity {
         lp2.weight = 1.0f;
 
         if(type == 1) {
-            lp2.gravity = Gravity.LEFT;
+            lp2.gravity = Gravity.RIGHT;
             textView.setBackgroundResource(R.drawable.bubble_in);
         }
         else{
-            lp2.gravity = Gravity.RIGHT;
+            lp2.gravity = Gravity.LEFT;
             textView.setBackgroundResource(R.drawable.bubble_out);
         }
         textView.setLayoutParams(lp2);

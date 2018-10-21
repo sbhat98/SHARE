54fb084f073cc81280593ccd5e6d0154352b0239
JoshMJ37 <jmjaffe37@gatech.edu>
2018-10-21 02:29:05 -0400

    Polishing Each activity

---
+++ app/src/main/java/edu/gatech/hackgt/effishare/Renter_List.java
@@ -0,0 +1,13 @@
+package edu.gatech.hackgt.effishare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Renter_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter__list);
+    }
+}
